7d84c8cbb2a2ffca0d618e57f1dcc5379b5ae2e5
DreamyStranger <98614364+DreamyStranger@users.noreply.github.com>
2023-05-10 05:56:06 -0400

    Complete Refactoring of WorldManager. Now, levels only using TiledCS

---
+++ GameManager/6. World Manager/LevelEnums.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace ECS_Framework
+{
+    public enum LevelID
+    {
+        Level1,
+        Level2,
+        // Add more level IDs here
+    }
+}
--- GameManager/6. World Manager/World.cs
+++ GameManager/6. World Manager/World.cs
@@ -10,8 +10,7 @@ namespace ECS_Framework
     /// </summary>
     public class World
     {
-        private LevelManager levelManager;
-        private Level currentLevel;
+        private LevelID currentLevel;
         private SystemManager systems;
         private int totalLevels = Enum.GetValues(typeof(LevelID)).Length;
 
@@ -22,37 +21,42 @@ namespace ECS_Framework
         /// </summary>
         public World()
         {
-            levelManager = new LevelManager();
-            CurrentLevel = levelManager.GetLevel(LevelID.Level1);
-            systems = new SystemManager(CurrentLevel.Id);
-            LoadLevel(CurrentLevel);
+            systems = new SystemManager(CurrentLevel);
+            entitiesToDestroy = new Queue<Entity>();
+
             MessageBus.Subscribe<DestroyEntityMessage>(OnDestroyEntity);
             MessageBus.Subscribe<NextLevelMessage>(NextLevel);
             MessageBus.Subscribe<ReloadLevelMessage>(ResetCurrentLevel);
-            entitiesToDestroy = new Queue<Entity>();
+
+            ChangeLevel(LevelID.Level1);
         }
 
         /// <summary>
-        /// Gets or sets the current level.
+        /// Gets the current level.
         /// </summary>
-        public Level CurrentLevel
+        public LevelID CurrentLevel
         {
             get { return currentLevel; }
-            set
-            {
-                currentLevel = value;
-                LoadLevel(currentLevel);
-            }
         }
 
         /// <summary>
-        /// Loads the given level and its entities into the game world.
+        /// Changes the current level to the specified level.
         /// </summary>
-        private void LoadLevel(Level level)
+        /// <param name="level">The level to change to.</param>
+        private void ChangeLevel(LevelID level)
+        {
+            currentLevel = level;
+            LoadLevel();
+        }
+
+        /// <summary>
+        /// Loads the current level and its entities into the game world.
+        /// </summary>
+        private void LoadLevel()
         {
             MediaPlayer.Stop();
-            systems.ResetSystems(level.Id);
-            List<Entity> objects = level.Initializer.GetObjects(currentLevel.Id);
+            systems.ResetSystems(CurrentLevel);
+            List<Entity> objects = LevelLoader.GetObjects(CurrentLevel);
             foreach (Entity entity in objects)
             {
                 systems.Add(entity);
@@ -62,18 +66,20 @@ namespace ECS_Framework
         /// <summary>
         /// Resets the current level by reloading it.
         /// </summary>
+        /// <param name="message">Optional message parameter.</param>
         public void ResetCurrentLevel(ReloadLevelMessage message = null)
         {
-            LoadLevel(CurrentLevel);
+            LoadLevel();
         }
 
         /// <summary>
         /// Advances to the next level.
         /// </summary>
+        /// <param name="message">Optional message parameter.</param>
         public void NextLevel(NextLevelMessage message = null)
         {
-            LevelID nextLevelID = GetNextLevelId(CurrentLevel.Id);
-            CurrentLevel = levelManager.GetLevel(nextLevelID);
+            currentLevel = (LevelID)(((int)currentLevel + 1 + totalLevels) % totalLevels);
+            LoadLevel();
         }
 
         /// <summary>
@@ -81,42 +87,23 @@ namespace ECS_Framework
         /// </summary>
         public void PreviousLevel()
         {
-            LevelID previousLevelID = GetPreviousLevelId(CurrentLevel.Id);
-            CurrentLevel = levelManager.GetLevel(previousLevelID);
-        }
-
-        /// <summary>
-        /// Gets the ID of the next level given the current level ID.
-        /// </summary>
-        private LevelID GetNextLevelId(LevelID currentLevelID)
-        {
-            int currentLevelIndex = (int)currentLevelID;
-            int nextLevelIndex = (currentLevelIndex + 1) % totalLevels;
-            return (LevelID)nextLevelIndex;
-        }
-
-        /// <summary>
-        /// Gets the ID of the previous level given the current level ID.
-        /// </summary>
-        private LevelID GetPreviousLevelId(LevelID currentLevelID)
-        {
-            int currentLevelIndex = (int)currentLevelID;
-            int previousLevelIndex = (currentLevelIndex - 1 + totalLevels) % totalLevels;
-            return (LevelID)previousLevelIndex;
+            currentLevel = (LevelID)(((int)currentLevel - 1 + totalLevels) % totalLevels);
+            LoadLevel();
         }
 
         /// <summary>
         /// Handles the destruction of entities.
         /// </summary>
+        /// <param name="message">The message containing the entity to destroy.</param>
         private void OnDestroyEntity(DestroyEntityMessage message)
         {
-            // Remove the entity from all systems
             entitiesToDestroy.Enqueue(message.Entity);
         }
 
         /// <summary>
         /// Updates the game world.
         /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public void Update(GameTime gameTime)
         {
             systems.Update(gameTime);
@@ -130,6 +117,7 @@ namespace ECS_Framework
         /// <summary>
         /// Draws the game world.
         /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch to draw with.</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             systems.Draw(spriteBatch);

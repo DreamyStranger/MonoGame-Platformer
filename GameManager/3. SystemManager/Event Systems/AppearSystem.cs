be0a36f61485fa43bdbdbfed179a2622ed4a3b65
DreamyStranger <98614364+DreamyStranger@users.noreply.github.com>
2023-05-10 21:56:22 -0400

    minor changes

--- GameManager/3. SystemManager/Event Systems/AppearSystem.cs
+++ GameManager/3. SystemManager/Event Systems/AppearSystem.cs
@@ -4,33 +4,52 @@ using System;
 
 namespace ECS_Framework
 {
+    /// <summary>
+    /// System that manages entity appearance events, triggering actions depending on the entity type.
+    /// </summary>
     public class AppearSystem : System
     {
         private List<Entity> _entities;
-        private List<Entity> _destroy;
+        private HashSet<Entity> _destroy;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppearSystem"/> class.
+        /// </summary>
         public AppearSystem()
         {
             _entities = new List<Entity>();
-            _destroy = new List<Entity>();
+            _destroy = new HashSet<Entity>();
         }
 
+        /// <summary>
+        /// Adds an entity to the system if it has both a StateComponent and an AnimatedComponent.
+        /// </summary>
+        /// <param name="entity">The entity to be added.</param>
         public override void AddEntity(Entity entity)
         {
-            if (entity.GetComponent<StateComponent>() != null && entity.GetComponent<AnimatedComponent>() != null)
+            if (entity.GetComponent<StateComponent>() == null || entity.GetComponent<AnimatedComponent>() == null)
             {
-                _entities.Add(entity);
+                return;
             }
+            _entities.Add(entity);
         }
 
+        /// <summary>
+        /// Removes an entity from the system.
+        /// </summary>
+        /// <param name="entity">The entity to be removed.</param>
         public override void RemoveEntity(Entity entity)
         {
             _entities.Remove(entity);
         }
 
+        /// <summary>
+        /// Updates the system, checking for entities in an IsAppearing state, and triggering the appropriate action.
+        /// </summary>
+        /// <param name="gameTime">The current game time.</param>
         public override void Update(GameTime gameTime)
         {
-            if(_entities.Count == 0)
+            if (_entities.Count == 0)
             {
                 //Console.WriteLine("IsEmpty!");  //Making sure the list is empty after entities appeared
                 return;
@@ -41,10 +60,12 @@ namespace ECS_Framework
                 StateComponent stateComponent = entity.GetComponent<StateComponent>();
                 AnimatedComponent animatedComponent = entity.GetComponent<AnimatedComponent>();
 
+                // Check if the entity is in the IsAppearing state
                 if (stateComponent.CurrentSuperState == SuperState.IsAppearing)
                 {
                     ActionAnimation appearAnimation = animatedComponent.GetCurrentAnimation();
 
+                    // Check if the animation has completed
                     if (appearAnimation.IsFinished)
                     {
                         _destroy.Add(entity);
@@ -53,13 +74,13 @@ namespace ECS_Framework
                 }
             }
 
-            //removed entities that already appeared
-            foreach(Entity entity in _destroy)
+            // Remove entities that have already appeared
+            foreach (Entity entity in _destroy)
             {
                 RemoveEntity(entity);
             }
 
-            //Clear out destroy List
+            // Clear out the destroy HashSet
             _destroy.Clear();
         }
     }
